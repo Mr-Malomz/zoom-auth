9b047b53ae7c757461d89403afbbb4d253a19b12
demola malomo <malomo.alaba@yahoo.com>
2022-05-31 09:15:52 +0100

    remove sensitive keys

--- helper/utils.js
+++ helper/utils.js
@@ -1,6 +1,6 @@
 import { Appwrite } from 'appwrite';
 
 const sdk = new Appwrite();
-sdk.setEndpoint('http://localhost/v1').setProject('6294eecca5821a6f92f9');
+sdk.setEndpoint('http://localhost/v1').setProject('ADD YOUR PROJECTID HERE');
 
 export default sdk;
